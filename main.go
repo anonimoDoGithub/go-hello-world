127ceb211fb5ea691379bce5185549dbb507c8c6
anonimoDoGithub <xxexpgamerxx@gmail.com>
2023-11-12 11:28:09 -0300

    primeiro ponto

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+// Tipos Primitivos
+var hi = "Hello, world!"
+var nome string = "Sergio"
+var sobrenome string = "Felipe"
+var idade int = 38
+var estudante bool = true
+
+func main(){
+	println(hi)
+	println("Olá,", nome, sobrenome, "!")
+	println("Você tem,", idade, "anos.")
+	println("Você está estudando, GoLang? ", estudante)
+}
+
